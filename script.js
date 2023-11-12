148af9f13652f8afc4a1cf734371bc3df8f6d648
GaneshS288 <swamiganesh288@gmail.com>
2023-11-12 21:02:41 +0530

    make basic function for calculations

---
+++ script.js
@@ -0,0 +1,47 @@
+let num1,
+num2,
+operator; 
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+
+    let result;
+
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+        break;
+        
+        case "-":
+            result = subtract(num1, num2);
+        break;
+        
+        case "*":
+            result = multiply(num1, num2);
+        break;
+
+        case "/":
+            result = divide(num1, num2);
+        break;
+
+        default:
+            return "error";        
+    }
+
+    return result;
+}
