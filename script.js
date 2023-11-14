8816e8f4792bc097f541ce3f3c9815888a65c91e
GaneshS288 <swamiganesh288@gmail.com>
2023-11-15 03:55:22 +0530

    make the number and operator buttons work and display on frontend

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-let num1,
-num2,
-operator; 
+const mathExpression = document.querySelector('.math-expression');
+const result = document.querySelector('.result');
+
+const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const delBtn = document.querySelector('.delete');
+const clearBtn = document.querySelector('.clear');
+
+numberBtns.forEach((button) => {
+    button.addEventListener("click", assignNumValues)
+});
+
+operatorBtns.forEach((button) => {
+    button.addEventListener("click", assignOperator)
+})
+
+function printExpression() {
+    mathExpression.textContent = `${num1}${operator}${num2}`
+}
+
+function assignNumValues(event) {
+    let num = event.target.textContent;
+    
+    if (!operator.length && num1.includes('.') && num1.length < 7) {
+        num1 += num;
+        printExpression();
+    }
+
+    else if (!operator.length && num1.length < 6) {
+        num1 += num
+        printExpression();
+    } 
+
+    else if (operator.length && num2.includes('.') && num2.length < 7) {
+        num2 += num;
+        printExpression();
+    }
+
+    else if (operator.length && num2.length < 6) {
+        num2 += num
+        printExpression();
+    }
+}
+
+function assignOperator(event) {
+    let opSign = event.target.textContent;
+
+    if (num1.length && !num2.length) {
+        operator = opSign
+        printExpression();
+    }
+
+    else if (num1.length && num2.length) {
+        operate(num1, operator, num2)
+    }
+}
+
+let num1 = '';
+let num2 = '';
+let operator = '';
 
 function add(num1, num2) {
     return num1 + num2;
@@ -18,7 +75,7 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, num2, operator) {
+function operate(num1, operator, num2) {
 
     let result;
 
@@ -42,7 +99,7 @@ function operate(num1, num2, operator) {
         default:
             return "error";        
     }
-
+    console.log(result)
     return result;
 }
 
